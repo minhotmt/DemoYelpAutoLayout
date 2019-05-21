71da3f4a671f5a7dc54dd5c049703dc4e088dbc9
minhnv <minhotmt@gmail.com>
2019-05-21 23:23:35 +0700

    Update get Api

--- DemoYelpAutoLayout/View Controller/MainViewController.swift
+++ DemoYelpAutoLayout/View Controller/MainViewController.swift
@@ -25,6 +25,7 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
         tableView.dataSource = self
         tableView.delegate = self
         lstBusiness = createBusiness()
+        loadData2()
     }
     
     func createBusiness() -> [Business]{
@@ -48,7 +49,6 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
         
     }
     
-    
     //TableView
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.lstBusiness.count
@@ -61,4 +61,65 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
         return cell
     }
     
+    func loadData2(){
+        let headers = [
+            "Authorization": "Bearer wpXNVIXNF39AhV8Qha-W2N8B-sKzHFmz5d9aRL6rXMb836jMZOvSOnfvYpW5Vzy6TAcuLbmKGU7DnwF1I8MPG_P7SRxQzTuD8LacU2IwGPcsjkL0ztlFXEzIAkXeXHYx",
+            "User-Agent": "PostmanRuntime/7.13.0",
+            "Accept": "*/*",
+            "Cache-Control": "no-cache",
+            "Postman-Token": "d469e39b-1f11-40dc-bee7-69f23c9580e7,bdf6b9f4-c6c8-439c-9b15-5b69e7636342",
+            "Host": "api.yelp.com",
+            "accept-encoding": "gzip, deflate",
+            "Connection": "keep-alive",
+            "cache-control": "no-cache"
+        ]
+        
+        let request = NSMutableURLRequest(url: NSURL(string: "https://api.yelp.com/v3/businesses/search?temp=Thai&latitude=37.774866&longitude=-122.394556")! as URL,
+                                          cachePolicy: .useProtocolCachePolicy,
+                                          timeoutInterval: 10.0)
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = headers
+        
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+           
+            guard let data = data else {
+                print("Error: No data to decode")
+                return
+            }
+            guard let blog = try? JSONDecoder().decode(Busines.self, from: data) else {
+                print("Error: Couldn't decode data into business")
+                return
+            }
+            
+            for item in blog.businesses {
+                print(item.location.address1!)
+            }
+            
+        })
+        
+        dataTask.resume()
+    }
+    
 }
+
+struct Busines: Decodable {
+    var businesses: [Busi]
+}
+
+struct Busi: Decodable {
+    let id: String
+    let alias: String
+    let name: String
+    let image_url: String
+    let review_count: Int
+    let rating: Float
+    let price: String
+    let location: Location
+}
+
+struct Location: Decodable {
+    let address1: String?
+    let address2: String?
+}
+
